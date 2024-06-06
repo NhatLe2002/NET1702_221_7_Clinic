abdb63f4beaffd60b1b1ef74adf8b2db7fb71028
NhatLe2002 <nhatlvmse160608@fpt.edu.vn>
2024-06-06 13:13:52 +0700

    WPF datagridview

--- ClinicWpfApp/MainWindow.xaml.cs
+++ ClinicWpfApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using ClinicBusiness;
+using ClinicData.Models;
+using ClinicWpfApp.UI.ClinicPage;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,51 @@ namespace ClinicWpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ClinicBusinessClass _business;
         public MainWindow()
         {
+            _business = new ClinicBusinessClass();
             InitializeComponent();
+            this.LoadGrdCurrencies();
+        }
+        private async void Open_wClinic_Click(object sender, RoutedEventArgs e)
+        {
+            var p = new wClinic();
+            p.Owner = this;
+            p.Show();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void grdClinic_MouseDouble_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void grdClinic_ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private async void LoadGrdCurrencies()
+        {
+            var result = await _business.GetAll();
+
+            if (result.Status > 0 && result.Data != null)
+            {
+                grdClinic.ItemsSource = result.Data as List<Clinic>;
+            }
+            else
+            {
+                grdClinic.ItemsSource = new List<Clinic>();
+            }
         }
     }
 }
